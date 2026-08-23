14c35fbe868a19e7677156d79d99d6165f5c8710
maneendhar.d@ifluids.com <maneendhar.d@ifluids.com>
2026-08-23 13:50:35 +0530

    Update test.py
    modified the test.py in the Document/github/main_project

--- test.py
+++ test.py
@@ -1,6 +1,44 @@
 import pandas as pd
 import numpy as np
 
-file = pd.read_excel(r"C:\Users\ifladmin\Desktop\Python Class\Mini_Project\FMDS.xlsx")
+file = pd.read_excel(r"C:\Users\ifladmin\Desktop\Python Class\Mini_Project\Facilitator Updates.xlsx")
 
-print(file)
+columns = [
+    "S_No",
+    "Date",
+    "Year",
+    "PR_No",
+    "Client",
+    "Client_Location",
+    "Department",
+    "Project_Title",
+    "Workshop_Status",
+    "Study",
+    "Facilitator",
+    "Scribe",
+    "Session_Date",
+    "WO_Number",
+    "Amount",
+    "Unused",
+    "Payment_Status"
+]
+
+file.columns = columns
+file = file.iloc[2:].copy()
+
+project_columns = [
+    "S_No",
+    "Date",
+    "Year",
+    "PR_No",
+    "Client",
+    "Client_Location",
+    "Department",
+    "Project_Title"
+]
+
+
+
+file[project_columns] = file[project_columns].ffill()
+
+print(file["Workshop_Status"].value_counts())
